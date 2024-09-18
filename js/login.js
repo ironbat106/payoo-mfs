8136da87e9d9c89fe5e602c68db41c381fcb40a5
ironbat106 <aryanahadateeq02@gmail.com>
2024-09-18 23:01:12 +0600

    added home page

--- js/login.js
+++ js/login.js
@@ -7,9 +7,24 @@ document.getElementById('button-login').addEventListener('click' , function(even
 
     //step 2: prevent default behavior (prevent reloading browser)
     event.preventDefault();
-    console.log('login button clicked');
 
     //step 3: get the phone number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+    const pinNumber = document.getElementById('pin-number').value;
+
+    console.log(phoneNumber, pinNumber)
+
+    //step 4: validate phone and pin
+    //this is temporary. don't do like this.
+
+    if(phoneNumber === '5' && pinNumber === '1234')
+    {
+        console.log('You are logged in!');
+        window.location.href = '/home.html';
+        //step 5: allow user to use website;
+    }
+    else
+    {
+        alert('Wrong credentials!');
+    }
 })
